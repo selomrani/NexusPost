f2ba9403cd90336cbb2779603ca1e7f24ccdb3cf
Soufyan <elomranisoufyan@gmail.com>
2026-01-28 11:44:05 +0100

    Project Redo

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+class Category extends Model
+{
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+}
